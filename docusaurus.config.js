cd7b06e746ef1018a84754b314a6d1d1bf1f5f0c
sethjm <sethemis@gmail.com>
2024-04-09 10:18:22 +0200

    Added algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,6 +82,36 @@ const config = {
     ({
       // Replace with your project's social card
       image: "img/logo-trabajos.jpg",
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "HC1PI5FDSC",
+
+        // Public API key: it is safe to commit it
+        apiKey: "f9f51b03a43e0e8d7380a24cf3f90bce",
+
+        indexName: "trabajosonline",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: "/docs/", // or as RegExp: /\/docs\//
+        //   to: "/",
+        // },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
+
       navbar: {
         title: "Trabajosonline Documentación",
         logo: {
